e20268882bd4e2b7793bea9edb6afa09d444b2b4
wesley <wesleyemidio99@hotmail.com>
2023-05-01 11:57:54 -0300

    Exercicio 1 finalizado

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
+void numerosImpares(int numero, int parada){
+    if(numero%2 == 0){
+        numero += 1;
+    } //verificação de numero par
+    if(parada != 0) {
+        printf("%d\n", numero);
+        numerosImpares(numero + 2, parada - 1);
+    }
+}
+
+
 int main() {
-    printf("Hello, World!\n");
+    //numerosImpares(2,10);
     return 0;
 }
