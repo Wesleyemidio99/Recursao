eb8b2589325ad065b5a2c4a682eb706049d105c0
wesley <wesleyemidio99@hotmail.com>
2023-05-02 23:37:07 -0300

    Ultimo exercicio finalizado

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+
+//Exercicio 1
 void numerosImpares(int numero, int parada){
     if(numero%2 == 0){
         numero += 1;
@@ -10,6 +12,7 @@ void numerosImpares(int numero, int parada){
     }
 }
 
+//Exercicio 2
 void exercicioDois(int numero,int parada){
     if(parada != 0){
         printf("%d\n",numero);
@@ -18,6 +21,8 @@ void exercicioDois(int numero,int parada){
     }
 }
 
+
+//Exercicio 3
 int numeroExponencial(int base, int expoente){
     if(expoente > 1){
         return base * numeroExponencial( base,expoente -1);
@@ -25,11 +30,25 @@ int numeroExponencial(int base, int expoente){
     return base;
 }
 
+
+//Exercicio 4
+double jurosComposto(double valor, double juros, int meses){
+    if(meses == 1) {
+        return valor * juros;
+    }
+    else{
+        return juros * jurosComposto(valor,juros,meses-1);
+
+    }
+}
+
 int main() {
 
     //numerosImpares(2,10);
     //exercicioDois(1,5);
-    printf("%d",numeroExponencial(2,5));
+    //printf("%d",numeroExponencial(2,5));
+    printf("%.2f",jurosComposto(1000.00,1.01,12));
+
     return 0;
 }
 
